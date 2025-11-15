4832c4c559c8bd7c254121f20063af221437a631
muscularbird <etienne.kretz@protonmail.com>
2025-11-15 23:12:43 +0100

    [~] unused variable

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
 
-
+  console.log(`Vite mode: ${mode}`);
   return (defineConfig({
     plugins: [
       react(),
